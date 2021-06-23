05f638331680da7de9fe87e3992e9f12b8b76f5d
Jörg Schönberger <office@webchills.at>
2021-06-23 08:11:52 +0200

    Anleitung aktualisiert

--- GEAENDERTE DATEIEN/includes/modules/pages/contact_us/header_php.php
+++ GEAENDERTE DATEIEN/includes/modules/pages/contact_us/header_php.php
@@ -3,7 +3,7 @@
  * Contact Us Page
  *
  * @package page
- * @copyright Copyright 2003-2019 Zen Cart Development Team
+ * @copyright Copyright 2003-2021 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
  * @version $Id: header_php.php for Google reCaptcha 2021-06-22 09:32:16Z webchills $
